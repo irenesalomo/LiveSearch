c33462e48ad1cf3dac570df87951e90cd72a3346
IreneSalomo <irene.salomo@gmail.com>
2018-07-04 17:24:55 +1000

    implement function for onkeyup

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,26 @@ var loadedUsers = 0;
 
 textBox.onkeyup = function(){
     var val = this.value;
-    val = val.replace(/^\s|\s+$/, "")
+    val = val.replace(/^\s+|\s+$/, ""); //This has better browser support than String.prototype.trim()
+
+    if (val != ""){
+        searchForData(val);
+    }
+    else{
+        clearResult();
+    }
+    // searchForData(val);
+}
+
+function searchForData(value, isLoadMoreMode){
+    // If an Ajax request is previously sent, it will be aborted because it isn't useful anymore.
+    // if(ajax && typeof ajax.abort === "function"){
+    //     ajax.abort();
+    // }
+
+    if(isLoadMoreMode !== true){
+        console.log("False");
+    }
+    else
+        console.log("True");
 }
