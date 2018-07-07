ae761f51a8e0acd352f50c41726c3c9f1c096cd4
Irene Salomo <irene.salomo@gmail.com>
2018-07-07 20:50:39 +1000

    complete searchForData, init showUsers function

--- src/js/app.js
+++ src/js/app.js
@@ -17,10 +17,11 @@ textBox.onkeyup = function(){
 }
 
 function searchForData(value, isLoadMoreMode){
-    // If an Ajax request is previously sent, it will be aborted because it isn't useful anymore.
-    if(ajax && typeof ajax.abort === "function"){
-        ajax.abort();
-    }
+    // If an Ajax request is previously sent, it will be aborted because it isn't useful anymore. This might not be necessary since Calling .open() method for an already active request (one for which open() has already been called) is the equivalent of calling abort().
+    
+    // if(ajax && typeof ajax.abort === "function"){
+    //     ajax.abort();
+    // }
 
     if(isLoadMoreMode !== true){
         clearResult();
@@ -28,8 +29,30 @@ function searchForData(value, isLoadMoreMode){
 
     ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function(){
-        
+        if (this.readyState === 4 && this.status === 200){
+            try{
+                var json = JSON.parse(this.responseText)
+            } catch (e){
+                noUsers();
+                return;
+            }
+
+            if(json.length ===0){
+                if(isLoadMoreMode){
+                    alert("No more to load");
+                } else{
+                    noUsers();
+                }
+            }
+            else{
+                showUsers(json);
+            }
+        }
     }
+    ajax.open('GET', 'search.php?username' + value + '&startFrom=' + loadedUsers , true);
+    ajax.send()
+}
 
-
+function showUsers(data){
+    
 }
